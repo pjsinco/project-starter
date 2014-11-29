e381ca15d826a8988d953aab5ea6ad8ca151a46b
Patrick Sinco <psinco@gmail.com>
2014-11-29 11:57:06 -0600

    add config.rb

---
+++ config.rb
@@ -0,0 +1,11 @@
+require 'susy'
+require 'compass-normalize'
+http_path = '/'
+css_dir = 'builds/dev/css'
+fonts_dir = 'builds/dev/fonts'
+sass_dir = 'components/sass'
+javascripts_dir = 'components/js'
+output_style = :nested
+images_dir = 'builds/dev/img'
+relative_assets = true
+line_comments = true
